4e6816f196abd853f7f0f75c6efa02f36bffd433
Hacker <74736605+HackerDevs@users.noreply.github.com>
2024-03-03 21:36:27 +0100

    Night vip (#8)
    * feat: Random VIP
    
    * code cleanup
    
    * add NightVIPData

---
+++ src/Models/NightVIPData.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VIP;
+public class NightVIPData
+{
+    [JsonPropertyName("enabled")]
+    public bool Enabled { get; set; } = false;
+
+    [JsonPropertyName("StartHour")]
+    public int StartHour { get; set; } = 3;
+
+    [JsonPropertyName("EndHour")]
+    public int EndHour { get; set; } = 3;
+
+    [JsonPropertyName("permissions")]
+    public List<string> Permissions { get; set; } = new List<string>();
+}
+
