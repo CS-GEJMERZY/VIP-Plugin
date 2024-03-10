cb1016ccd9b2f59b995bbebcd7b6438e0279c661
Hacker <74736605+HackerDevs@users.noreply.github.com>
2024-03-10 22:13:51 +0100

    fix: Random VIP winner not being displayed

--- src/Managers/RandomVipManager.cs
+++ src/Managers/RandomVipManager.cs
@@ -6,41 +6,40 @@ namespace Plugin.Managers
 {
     public class RandomVipManager
     {
-        internal Models.RandomVIPData randomVipData { get; set; }
+        internal Models.RandomVIPData RandomVipData { get; set; }
 
         public RandomVipManager(Models.RandomVIPData randomVipData)
         {
-            this.randomVipData = randomVipData;
+            RandomVipData = randomVipData;
         }
 
         public bool IsRound(int RoundNumber)
         {
-            return RoundNumber == randomVipData.AfterRound;
+            return RoundNumber == RandomVipData.AfterRound;
         }
 
         public void ProcessRound(IStringLocalizer Localizer)
         {
             var players = PlayerManager.GetValidPlayers().Where(
-                player => Managers.PermissionManager.HasAnyPermission(player, randomVipData.PermissionExclude)).ToList();
+                player => PermissionManager.HasAnyPermission(player, RandomVipData.PermissionExclude)).ToList();
             if (players.Count == 0 ||
-                players.Count < randomVipData.MinimumPlayers) { return; }
+                players.Count < RandomVipData.MinimumPlayers) { return; }
 
             var randomPlayer = ChooseRandomPlayer(players);
 
             AnnouncePickingProcess(Localizer);
-            PermissionManager.AddPermissions(randomPlayer, randomVipData.PermissionsGranted);
+            PermissionManager.AddPermissions(randomPlayer, RandomVipData.PermissionsGranted);
             AnnounceWinner(randomPlayer, Localizer);
-
         }
 
-        public void AnnounceWinner(CCSPlayerController player, IStringLocalizer Localizer)
+        public static void AnnounceWinner(CCSPlayerController player, IStringLocalizer Localizer)
         {
             Server.PrintToChatAll(Localizer["winner", player.PlayerName]);
         }
 
         private void AnnouncePickingProcess(IStringLocalizer Localizer)
         {
-            for (int i = 0; i < randomVipData.RepeatPicking; i++)
+            for (int i = 0; i < RandomVipData.RepeatPicking; i++)
             {
                 Server.PrintToChatAll(Localizer["picking"]);
             }
